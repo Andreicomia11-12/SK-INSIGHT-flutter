2eb1292a08796cb8365896a1ce310c4cbab2cf14
margiecutie <margiecutie09@gmail.com>
2025-06-26 11:36:19 +0800

    login1

--- lib/pages/forgot_pass.dart
+++ lib/pages/forgot_pass.dart
@@ -1,71 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:skinsight/pages/forgotpass1.dart';
 
-
 class Forgot extends StatelessWidget {
+  const Forgot({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Column(
           children: [
+            Container(child: Image.asset('assets/sk.jpg')),
             Container(
-              child: Image.asset(
-                'assets/sk.jpg',
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [Container(child: Text('Forgot Your Password'))],
               ),
-
             ),
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Forgot Your Password'),
-                )
-              ],
-            ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Enter your email to reset your password'),
-                )
-              ],
-            )
+                children: [
+                  Container(
+                    child: Text('Enter your email to reset your password'),
+                  ),
+                ],
+              ),
             ),
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Email: __________________'),
-                )
-              ],
-            )
+                children: [Container(child: Text('Email: __________________'))],
+              ),
             ),
             Container(
               child: Column(
-                  children: [
-                      Container(
-                       child: ElevatedButton(onPressed: (){
+                children: [
+                  Container(
+                    child: ElevatedButton(
+                      onPressed: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(builder: (context) => Forgot1()),
                         );
-
-                       }, child: Text('Send Reset Link'),)
-                      ),
-                    ]
-                  )
-                 ),
+                      },
+                      child: Text('Send Reset Link'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
--- lib/pages/forgotpass1.dart
+++ lib/pages/forgotpass1.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Forgot1 extends StatelessWidget {
-  const Forgot1({Key? key}) : super(key: key);
+  const Forgot1({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,56 +9,47 @@ class Forgot1 extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
+            Container(child: Image.asset('assets/sk.jpg')),
             Container(
-              child: Image.asset(
-                'assets/sk.jpg',
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [Container(child: Text('Check your Email'))],
               ),
-
             ),
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Check your Email'),
-                )
-              ],
-            ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('If an account with that email exist, a reset link has been sent'),
-                )
-              ],
-            )
+                children: [
+                  Container(
+                    child: Text(
+                      'If an account with that email exist, a reset link has been sent',
+                    ),
+                  ),
+                ],
+              ),
             ),
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Please check your inbox or spam folder'),
-                )
-              ],
-            )
+                children: [
+                  Container(
+                    child: Text('Please check your inbox or spam folder'),
+                  ),
+                ],
+              ),
             ),
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Go back to'),
-                ),
-                ElevatedButton(onPressed: () {}, child: Text('Log in'))
-              ],
-            )
+                children: [
+                  Container(child: Text('Go back to')),
+                  ElevatedButton(onPressed: () {}, child: Text('Log in')),
+                ],
+              ),
             ),
           ],
         ),
